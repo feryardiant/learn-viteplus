ee5440678931ee67f4b2b1bd59f90647677dafe7
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-06-19 13:30:48 +0700

    test: another rearrangement test configs
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- packages/utils/vite.config.ts
+++ packages/utils/vite.config.ts
@@ -1,19 +1,28 @@
-import { defineConfig } from 'vite-plus'
+import { defineConfig, loadEnv, type TestUserConfig, type UserConfig } from 'vite-plus'
 
-export default defineConfig({
-  pack: {
-    dts: {
-      tsgo: true,
+export default defineConfig(({ mode }): UserConfig => {
+  const env = loadEnv(mode, '.', '')
+  const reporters: TestUserConfig['reporters'] = ['default', ['junit', { outputFile: 'tests/reports/junit.xml' }]]
+
+  if (env.GITHUB_ACTIONS === 'true') {
+    reporters.push('github-actions')
+  }
+
+  return {
+    pack: {
+      dts: {
+        tsgo: true,
+      },
+      exports: true,
     },
-    exports: true,
-  },
-  test: {
-    include: ['tests/units/*.spec.ts', 'tests/integrations/*.spec.ts'],
-    reporters: ['default', ['junit', { outputFile: 'tests/reports/junit.xml' }]],
-    coverage: {
-      provider: 'istanbul',
-      include: ['src/**'],
-      reportsDirectory: 'tests/reports/coverage',
+    test: {
+      include: ['tests/units/*.spec.ts', 'tests/integrations/*.spec.ts'],
+      reporters,
+      coverage: {
+        provider: 'istanbul',
+        include: ['src/**'],
+        reportsDirectory: 'tests/reports/coverage',
+      },
     },
-  },
+  }
 })
